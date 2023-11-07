a685f3c221786dc4f0c64b8ba403205a22e58c0b
g <spamargsr@gmail.com>
2023-11-07 14:24:20 -0300

    add footer

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+
+    return (
+        <footer className="bg-stone-600 text-slate-200 text-sm pt-3 pb-2 text-center">
+            <p>©Whimsy Cookie Workshop. </p>
+            <p>
+                Made by <a className="hover:text-white hover:underline" href="https://www.gustavosasaki.com/">Gustavo Sasaki</a>
+            </p>
+        </footer>
+    );
+}
+
