6eb3cbde7b605521426e13a7243a7b41e3aea2b8
g <spamargsr@gmail.com>
2023-11-07 14:19:24 -0300

    add about section

--- src/components/introduction/ReadMoreButton.tsx
+++ src/components/introduction/ReadMoreButton.tsx
@@ -1,9 +1,18 @@
+import { useContext } from "react"
+import { AboutRefContext } from "../hero/NavContextProvider"
+import { scrollToPosition } from "../hero/scrollToPosition"
+
 export function ReadMoreButton() {
+
+    const aboutRef = useContext(AboutRefContext)
+    
     return (
         <div className="flex justify-center items-center relative">
             <div className="relative group  w-full max-w-[400px] ">
-                <button className="relative w-full rounded-xl bg-gradient-to-br from-introduction to-introduction-lighter text-white py-2 z-20"
+                <button 
+                    className="relative w-full rounded-xl bg-gradient-to-br from-introduction to-introduction-lighter text-white py-2 z-20"
                     aria-label="read more about Whimsy Cookie Workshop"
+                    onClick={(event) => scrollToPosition(aboutRef)}
                 >
                     <p>Read More</p>
                 </button>
